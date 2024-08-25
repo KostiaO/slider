7d6393435dfb89fa9a1cfb3f9840d7bffcb0ed07
KostiaO <kostiaomelcenko@gmail.com>
2024-08-25 22:48:43 +0200

    erste unstable

---
+++ README.md
@@ -0,0 +1,74 @@
+# slider
+#### Made with create-jquery-plugin
+
+[![npm version](https://badge.fury.io/js/slider.svg)](https://www.npmjs.com/package/slider)
+
+Requirements
+-----
+  + [jQuery](http://jquery.com/)
+
+
+Installation
+-----
+
+### [NPM](https://www.npmjs.com/package/slider)
+```bash
+npm install slider
+```
+
+### [Yarn](https://yarn.pm/slider)
+```bash
+yarn add slider
+```
+
+### [CDN - jsDelivr](https://www.jsdelivr.com/package/npm/slider)
+```html
+<!-- CSS -->
+<link href="https://cdn.jsdelivr.net/npm/slider/dist/css/slider.min.css" rel="stylesheet" type="text/css" />
+
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/slider/dist/js/slider.min.js" type="text/javascript"></script>
+```    
+
+### [CDN - UNPKG](https://unpkg.com/browse/slider/)
+```html
+<!-- CSS -->
+<link href="https://unpkg.com/slider/dist/css/slider.min.css" rel="stylesheet" type="text/css" />
+
+<!-- JavaScript -->
+<script src="https://unpkg.com/slider/dist/js/slider.min.js" type="text/javascript"></script>
+```
+
+Usage
+-----
+
+Include CSS
+```html
+<link href="https://cdn.jsdelivr.net/npm/slider/dist/css/slider.min.css" rel="stylesheet" type="text/css" />
+```
+
+HTML
+```html
+<div id="element"></div>
+```
+
+Include jQuery
+```html
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+```
+
+Include Plugin JS
+```html
+<script src="https://cdn.jsdelivr.net/npm/slider/dist/js/slider.min.js" type="text/javascript"></script>
+```
+Initialize the Plugin
+```js
+$('#element').slider();
+```
+
+License
+----
+MIT
+
+
+Created with :heart: [create-jquery-plugin](https://www.npmjs.com/package/create-jquery-plugin)
---
+++ src/js/slider.js
@@ -0,0 +1,115 @@
+/*!
+ * slider 1.0.0
+ * Made with create-jquery-plugin
+ *
+ * Created by KostiaO <kostiaomelcenko@gmail.com>
+ *
+ * @license MIT
+ */
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+   class Slider {
+
+    constructor(element, data) {
+
+        this.data = data.map((entity, index) => {
+            return {
+                ...entity,
+                id: `${entity.btnName}${index}`
+            };
+        });
+
+        this.currentSlide = 0;
+
+        this.selector = $(element).attr('class');
+
+        // Main container element
+        this.main = $(`.${this.selector}`);
+
+        // Initial load
+        this._init();
+    }
+    _init(){
+        this.slider();
+    }
+    slider(){
+       this.initSlider();
+    }
+
+    initSlider() {
+        this.setSliderImage(this.data[this.currentSlide].bcgUrl);
+
+        this.initBtns();
+
+        for (let i = 0; i < this.data.length; i++) {
+           $(`#${this.data[i].id}`).on("click", () => {
+               this.handleClassRemoveActive(this.currentSlide);
+               this.changeSlide(i);
+               this.handleClassSetActive(i);
+               this.setSliderImage(this.data[this.currentSlide].bcgUrl);
+           });
+
+           this.handleClassSetActive(i);
+        }
+    }
+
+    initBtns() {
+        const buttons = this.data.map((entity) => {
+            return `<button class="${entity.btnClassName}" id="${entity.id}">${entity.btnName}</button>`;
+        });
+
+        this.main.append(`<div class="${this.selector}_buttons">${buttons.join('')}</div>`);
+    }
+
+    changeSlide(slideNum) {
+       this.currentSlide = slideNum;
+    }
+
+    handleClassSetActive(index) {
+       if (index === this.currentSlide) {
+           $(`#${this.data[index].id}`).addClass('is-active');
+       }
+    }
+
+    handleClassRemoveActive(index) {
+       $(`#${this.data[index].id}`).removeClass('is-active');
+    }
+
+    setSliderImage(url) {
+       this.main.css('background-image', 'url(' + url + ')');
+    }
+   }
+
+   // Wrapper for the plugin
+   $.fn.slider = function (data) {
+      new Slider(this, data);
+
+      return this;
+   };
+}));
